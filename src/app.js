2bd4a1da60efb38d7e86790736e91a7ec8a947a6
jayakumar0003 <jayakumar.2022891994@gmail.com>
2026-05-12 23:38:04 +0530

    Fixed CORS Error

--- src/app.js
+++ src/app.js
@@ -15,14 +15,15 @@ app.use(
     origin: [
       "http://localhost:3000",
       "http://localhost:8080",
-      "https://targetingandanalytics-frontend.onrender.com"
+      "https://targetingandanalytics-frontend.onrender.com",
+      "https://ssot-wpp.vercel.app/"
     ],
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true
   })
 );
 
-  
+
 app.use(express.json());
 
 app.use("/api/targeting", targetingRoutes);
